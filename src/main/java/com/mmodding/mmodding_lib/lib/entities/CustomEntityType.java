ccdab1b8250e479d0a1f8d5d63d06733d13e2ea6
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-05-15 15:16:50 +0200

    Change CustomEntityType and EntityTypeRegistrable

--- src/main/java/com/mmodding/mmodding_lib/lib/entities/CustomEntityType.java
+++ src/main/java/com/mmodding/mmodding_lib/lib/entities/CustomEntityType.java
@@ -1,20 +1,20 @@
 package com.mmodding.mmodding_lib.lib.entities;
 
 import com.google.common.collect.ImmutableSet;
+import net.fabricmc.fabric.impl.object.builder.FabricEntityType;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class CustomEntityType<T extends Entity> extends EntityType<T> implements EntityTypeRegistrable {
+public class CustomEntityType<T extends Entity> extends FabricEntityType<T> implements EntityTypeRegistrable<T> {
 
 	private final AtomicBoolean registered = new AtomicBoolean(false);
 
-	public CustomEntityType(EntityFactory<T> entityFactory, SpawnGroup spawnGroup, boolean bl, boolean bl2, boolean bl3, boolean bl4, ImmutableSet<Block> immutableSet, EntityDimensions entityDimensions, int i, int j) {
-		super(entityFactory, spawnGroup, bl, bl2, bl3, bl4, immutableSet, entityDimensions, i, j);
+	public CustomEntityType(EntityFactory<T> factory, SpawnGroup spawnGroup, boolean bl, boolean summonable, boolean fireImmune, boolean spawnableFarFromPlayer, ImmutableSet<Block> spawnBlocks, EntityDimensions entityDimensions, int maxTrackDistance, int trackTickInterval, Boolean alwaysUpdateVelocity) {
+		super(factory, spawnGroup, bl, summonable, fireImmune, spawnableFarFromPlayer, spawnBlocks, entityDimensions, maxTrackDistance, trackTickInterval, alwaysUpdateVelocity);
 	}
 
 	@Override
--- src/main/java/com/mmodding/mmodding_lib/lib/entities/EntityTypeRegistrable.java
+++ src/main/java/com/mmodding/mmodding_lib/lib/entities/EntityTypeRegistrable.java
@@ -2,13 +2,14 @@ package com.mmodding.mmodding_lib.lib.entities;
 
 import com.mmodding.mmodding_lib.lib.utils.Registrable;
 import com.mmodding.mmodding_lib.lib.utils.RegistrationUtils;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 
-public interface EntityTypeRegistrable extends Registrable {
+public interface EntityTypeRegistrable<T extends Entity> extends Registrable {
 
 	default void register(Identifier identifier) {
-		if (this instanceof CustomEntityType<?> && this.isNotRegistered()) {
-			RegistrationUtils.registerEntityType(identifier, (CustomEntityType<?>) this);
+		if (this instanceof CustomEntityType<T> && this.isNotRegistered()) {
+			RegistrationUtils.registerEntityType(identifier, (CustomEntityType<T>) this);
 		}
 	}
 }
