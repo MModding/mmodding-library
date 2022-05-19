4b9ded02000817c9038c93dbfae8f96fce7babf8
Max094Reikeb <maxencebekier@gmail.com>
2022-05-19 18:57:20 +0200

    Modified changeBiome method

--- src/main/java/com/mmodding/mmodding_lib/lib/utils/BiomeUtils.java
+++ src/main/java/com/mmodding/mmodding_lib/lib/utils/BiomeUtils.java
@@ -2,12 +2,21 @@ package com.mmodding.mmodding_lib.lib.utils;
 
 import net.minecraft.util.Holder;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.HeightLimitView;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkSection;
 
 public class BiomeUtils {
 
-	public static void changeBiomeForBlock(BlockPos pos, ChunkSection section, Biome biome) {
-		section.getBiomeContainer().set(pos.getX(), pos.getY(), pos.getZ(), Holder.createDirect(biome));
+	public static void changeBiomeForBlock(World world, BlockPos pos, ChunkSection section, Biome biome) {
+		Chunk chunk = world.getChunk(pos);
+		HeightLimitView heightLimitView = chunk.getHeightLimitView();
+
+		for (int k = heightLimitView.getBottomSectionCoord(); k < heightLimitView.getTopSectionCoord(); k++) {
+			ChunkSection chunkSection = chunk.getSection(chunk.sectionCoordToIndex(k));
+			chunkSection.getBiomeContainer().set(pos.getX(), pos.getY(), pos.getZ(), Holder.createDirect(biome));
+		}
 	}
 }
