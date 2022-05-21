6dc98cabab4f533f1ec906acea612332ada55775
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-05-21 20:01:58 +0200

    Fix RadiusUtils#forBlockPosInCubicRadius

--- src/main/java/com/mmodding/mmodding_lib/lib/utils/RadiusUtils.java
+++ src/main/java/com/mmodding/mmodding_lib/lib/utils/RadiusUtils.java
@@ -8,8 +8,8 @@ public class RadiusUtils {
 
 	public static void forBlockPosInCubicRadius(BlockPos pos, int radius, BlockPosToRunnable blockPosToRunnable) {
 		for (int x: IntStream.range(pos.getX() - radius, pos.getX() + radius).toArray()) {
-			for (int y: IntStream.range(pos.getY() + radius, pos.getY() - radius).toArray()) {
-				for (int z: IntStream.range(pos.getZ() + radius, pos.getZ() - radius).toArray()) {
+			for (int y: IntStream.range(pos.getY() - radius, pos.getY() + radius).toArray()) {
+				for (int z: IntStream.range(pos.getZ() - radius, pos.getZ() + radius).toArray()) {
 					blockPosToRunnable.execute(new BlockPos(x, y ,z));
 				}
 			}
