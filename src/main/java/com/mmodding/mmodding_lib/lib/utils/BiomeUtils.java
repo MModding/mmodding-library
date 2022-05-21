386143a1124d84bb55f821785c36028e3c25b373
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-05-22 00:01:09 +0200

    Fix #1

--- src/main/java/com/mmodding/mmodding_lib/lib/utils/BiomeUtils.java
+++ src/main/java/com/mmodding/mmodding_lib/lib/utils/BiomeUtils.java
@@ -2,20 +2,17 @@ package com.mmodding.mmodding_lib.lib.utils;
 
 import net.minecraft.util.Holder;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.ChunkSection;
 
 public class BiomeUtils {
 
 	public static void changeBiomeForBlock(World world, BlockPos pos, Biome biome) {
 		Chunk chunk = world.getChunk(pos);
-		ChunkSectionPos sectionPos = ChunkSectionPos.from(new ChunkPos(pos), ChunkSectionPos.getSectionCoord(pos.getY()));
-
-		ChunkSection chunkSection = chunk.getSection(chunk.getSectionIndex(pos.getY()));
-		chunkSection.getBiomeContainer().setSync(sectionPos.getX(),	sectionPos.getY(), sectionPos.getZ(), Holder.createDirect(biome));
+		chunk.getSection(chunk.getSectionIndex(pos.getY())).getBiomeContainer().setUnsafe(
+				pos.getX() & 3, pos.getY() & 3, pos.getZ() & 3, Holder.createDirect(biome)
+		);
+		chunk.setNeedsSaving(true);
 	}
 }
