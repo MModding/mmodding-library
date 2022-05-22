6aeb5f2d6556c2c4b500724348ab1ebbb190cfef
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-05-22 02:03:23 +0200

    Change RadiusUtils

--- src/main/java/com/mmodding/mmodding_lib/lib/utils/RadiusUtils.java
+++ src/main/java/com/mmodding/mmodding_lib/lib/utils/RadiusUtils.java
@@ -2,21 +2,18 @@ package com.mmodding.mmodding_lib.lib.utils;
 
 import net.minecraft.util.math.BlockPos;
 
-import java.util.stream.IntStream;
+import java.util.function.Consumer;
 
 public class RadiusUtils {
 
-	public static void forBlockPosInCubicRadius(BlockPos pos, int radius, BlockPosToRunnable blockPosToRunnable) {
-		for (int x: IntStream.range(pos.getX() - radius, pos.getX() + radius).toArray()) {
-			for (int y: IntStream.range(pos.getY() - radius, pos.getY() + radius).toArray()) {
-				for (int z: IntStream.range(pos.getZ() - radius, pos.getZ() + radius).toArray()) {
-					blockPosToRunnable.execute(new BlockPos(x, y ,z));
-				}
-			}
-		}
-	}
-
-	public interface BlockPosToRunnable {
-		void execute(BlockPos pos);
+	public static void forBlockPosInCubicRadius(BlockPos pos, int radius, Consumer<? super BlockPos> execute) {
+		BlockPos.iterate(
+				pos.getX() - radius,
+				pos.getY() - radius,
+				pos.getZ() - radius,
+				pos.getX() + radius,
+				pos.getY() + radius,
+				pos.getZ() + radius
+		).forEach(execute);
 	}
 }
