51212c7cb537e36d43fc6cc2e14aa804d40e8860
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-05-25 16:42:53 +0200

    Change BiomeUtils#changeBiomeForBlock

--- src/main/java/com/mmodding/mmodding_lib/library/utils/BiomeUtils.java
+++ src/main/java/com/mmodding/mmodding_lib/library/utils/BiomeUtils.java
@@ -4,14 +4,13 @@ import net.minecraft.util.Holder;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 
 public class BiomeUtils {
 
-	public static void changeBiomeForBlock(World world, BlockPos pos, Biome biome) {
+	public static void changeBiomeForBlock(WorldAccess world, BlockPos pos, Biome biome) {
 		Chunk chunk = world.getChunk(pos);
 		chunk.getSection(chunk.getSectionIndex(pos.getY())).getBiomeContainer().setUnsafe(
 				pos.getX() & 3, pos.getY() & 3, pos.getZ() & 3, Holder.createDirect(biome)
