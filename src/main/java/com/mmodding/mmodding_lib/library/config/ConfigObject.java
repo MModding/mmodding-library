d9e0159d5733ea358fbfcda57cdc1e0927ff4a49
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-08-22 00:26:52 +0200

    Update ConfigObject

--- src/main/java/com/mmodding/mmodding_lib/library/config/ConfigObject.java
+++ src/main/java/com/mmodding/mmodding_lib/library/config/ConfigObject.java
@@ -14,25 +14,25 @@ public class ConfigObject {
 		this.jsonObject = object;
 	}
 
-	public String getString(String parameter) throws FileNotFoundException {
+	public String getString(String parameter) {
 		return this.jsonObject.get(parameter).getAsString();
 	}
 
-	public int getInteger(String parameter) throws FileNotFoundException {
+	public int getInteger(String parameter) {
 		return this.jsonObject.get(parameter).getAsInt();
 	}
 
-	public boolean getBoolean(String parameter) throws FileNotFoundException {
+	public boolean getBoolean(String parameter) {
 		return this.jsonObject.get(parameter).getAsBoolean();
 	}
 
-	public List<Object> getArray(String parameter) throws FileNotFoundException {
+	public List<Object> getArray(String parameter) {
 		List<Object> list = new ArrayList<>();
 		this.jsonObject.getAsJsonArray(parameter).forEach(list::add);
 		return list;
 	}
 
-	public ConfigObject getCategory(String parameter) throws FileNotFoundException {
+	public ConfigObject getCategory(String parameter) {
 		return new ConfigObject(this.jsonObject.getAsJsonObject(parameter));
 	}
 }
