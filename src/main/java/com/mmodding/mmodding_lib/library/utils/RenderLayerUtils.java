19531f934758a3e149aa053b5221a87bd468d768
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-08-29 17:53:54 +0200

    Change RenderLayerUtils

--- src/main/java/com/mmodding/mmodding_lib/library/utils/RenderLayerUtils.java
+++ src/main/java/com/mmodding/mmodding_lib/library/utils/RenderLayerUtils.java
@@ -1,6 +1,9 @@
 package com.mmodding.mmodding_lib.library.utils;
 
+import com.mmodding.mmodding_lib.mixin.BufferBuilderStorageAccessor;
 import net.minecraft.block.Block;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.RenderLayer;
 import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap;
 
@@ -12,4 +15,8 @@ public class RenderLayerUtils {
     public static void setTranslucent(Block block) {
 		BlockRenderLayerMap.put(RenderLayer.getTranslucent(), block);
     }
+
+	public void addEntityBuilder(RenderLayer layer) {
+		((BufferBuilderStorageAccessor) MinecraftClient.getInstance().getBufferBuilders()).getEntityBuilders().put(layer, new BufferBuilder(layer.getExpectedBufferSize()));
+	}
 }
