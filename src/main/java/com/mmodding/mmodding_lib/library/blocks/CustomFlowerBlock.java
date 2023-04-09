c96d0ba25fd8c4c69ea406bb655b8e457ae81e6a
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-04-09 23:00:59 +0200

    Fix CustomFlowerBlock and CustomPlantBlock

--- src/main/java/com/mmodding/mmodding_lib/library/blocks/CustomFlowerBlock.java
+++ src/main/java/com/mmodding/mmodding_lib/library/blocks/CustomFlowerBlock.java
@@ -1,16 +1,34 @@
 package com.mmodding.mmodding_lib.library.blocks;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.FlowerBlock;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
 
 public class CustomFlowerBlock extends FlowerBlock implements BlockRegistrable {
+
+	private final Predicate<BlockState> placementConditions;
 
 	private final AtomicBoolean registered = new AtomicBoolean(false);
 
 	public CustomFlowerBlock(StatusEffect suspiciousStewEffect, int effectDuration, Settings settings) {
+		this(floor -> floor.isIn(BlockTags.DIRT) || floor.isOf(Blocks.FARMLAND), suspiciousStewEffect, effectDuration, settings);
+	}
+
+	public CustomFlowerBlock(Predicate<BlockState> placementConditions, StatusEffect suspiciousStewEffect, int effectDuration, Settings settings) {
 		super(suspiciousStewEffect, effectDuration, settings);
+		this.placementConditions = placementConditions;
+	}
+
+	@Override
+	protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+		return this.placementConditions.test(floor);
 	}
 
 	@Override
--- src/main/java/com/mmodding/mmodding_lib/library/blocks/CustomPlantBlock.java
+++ src/main/java/com/mmodding/mmodding_lib/library/blocks/CustomPlantBlock.java
@@ -1,15 +1,34 @@
 package com.mmodding.mmodding_lib.library.blocks;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.PlantBlock;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldView;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
 
 public class CustomPlantBlock extends PlantBlock implements BlockRegistrable {
+
+	private final Predicate<BlockState> placementConditions;
 
 	private final AtomicBoolean registered = new AtomicBoolean(false);
 
 	public CustomPlantBlock(Settings settings) {
+		this(floor -> floor.isIn(BlockTags.DIRT) || floor.isOf(Blocks.FARMLAND), settings);
+	}
+
+	public CustomPlantBlock(Predicate<BlockState> placementConditions, Settings settings) {
 		super(settings);
+		this.placementConditions = placementConditions;
+	}
+
+	@Override
+	protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+		return this.placementConditions.test(floor);
 	}
 
 	@Override
