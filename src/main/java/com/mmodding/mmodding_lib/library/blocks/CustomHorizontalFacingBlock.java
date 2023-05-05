5cff5961f841a361ee65ab68b6fd0a085760167e
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-05-05 22:06:31 +0200

    Add CustomHorizontalFacingBLock

---
+++ src/main/java/com/mmodding/mmodding_lib/library/blocks/CustomHorizontalFacingBlock.java
@@ -0,0 +1,63 @@
+package com.mmodding.mmodding_lib.library.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import org.jetbrains.annotations.Nullable;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CustomHorizontalFacingBlock extends HorizontalFacingBlock implements BlockRegistrable, BlockWithItem {
+
+    private final AtomicBoolean registered = new AtomicBoolean(false);
+    private BlockItem item = null;
+
+    public CustomHorizontalFacingBlock(Settings settings) {
+        this(settings, false);
+    }
+
+    public CustomHorizontalFacingBlock(Settings settings, boolean hasItem) {
+        this(settings, hasItem, (ItemGroup) null);
+    }
+
+	public CustomHorizontalFacingBlock(Settings settings, boolean hasItem, ItemGroup itemGroup) {
+		this(settings, hasItem, itemGroup != null ? new QuiltItemSettings().group(itemGroup) : new QuiltItemSettings());
+	}
+
+    public CustomHorizontalFacingBlock(Settings settings, boolean hasItem, Item.Settings itemSettings) {
+        super(settings);
+        if (hasItem) this.item = new BlockItem(this, itemSettings);
+    }
+
+	@Nullable
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+
+	@Override
+    public BlockItem getItem() {
+        return this.item;
+    }
+
+    @Override
+    public boolean isNotRegistered() {
+        return !this.registered.get();
+    }
+
+    @Override
+    public void setRegistered() {
+        this.registered.set(true);
+    }
+}
