cae2e7303cc01787965f86819237d4d69d79ed57
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-05-08 17:11:07 +0200

    Add CustomMusicDiscItem

---
+++ src/main/java/com/mmodding/mmodding_lib/library/items/CustomMusicDiscItem.java
@@ -0,0 +1,25 @@
+package com.mmodding.mmodding_lib.library.items;
+
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.sound.SoundEvent;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CustomMusicDiscItem extends MusicDiscItem implements ItemRegistrable {
+
+	private final AtomicBoolean registered = new AtomicBoolean(false);
+
+	public CustomMusicDiscItem(Settings settings, SoundEvent soundEvent, int comparatorOutput, int seconds) {
+		super(comparatorOutput, soundEvent, settings, seconds);
+	}
+
+	@Override
+	public boolean isNotRegistered() {
+		return !registered.get();
+	}
+
+	@Override
+	public void setRegistered() {
+		registered.set(true);
+	}
+}
