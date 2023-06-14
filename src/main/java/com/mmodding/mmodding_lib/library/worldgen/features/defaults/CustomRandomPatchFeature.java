69051a4956a949f508acb0c07fdb8bb1e9025d4a
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-06-15 00:20:19 +0200

    FIx CustomRandomPatchFeature

--- src/main/java/com/mmodding/mmodding_lib/library/worldgen/features/defaults/CustomRandomPatchFeature.java
+++ src/main/java/com/mmodding/mmodding_lib/library/worldgen/features/defaults/CustomRandomPatchFeature.java
@@ -1,8 +1,11 @@
 package com.mmodding.mmodding_lib.library.worldgen.features.defaults;
 
 import net.minecraft.util.Holder;
+import net.minecraft.world.gen.decorator.BiomePlacementModifier;
+import net.minecraft.world.gen.decorator.CountPlacementModifier;
+import net.minecraft.world.gen.decorator.InSquarePlacementModifier;
 import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.feature.util.ConfiguredFeatureUtil;
+import net.minecraft.world.gen.feature.util.PlacedFeatureUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +15,14 @@ public class CustomRandomPatchFeature implements CustomFeature, FeatureRegistrab
 
 	private final AtomicBoolean registered = new AtomicBoolean();
 
+	private final int count;
 	private final int tries;
 	private final int spreadXZ;
 	private final int spreadY;
 	private final Holder<PlacedFeature> feature;
 
-	public CustomRandomPatchFeature(int tries, int spreadHorizontally, int spreadVertically, Holder<PlacedFeature> feature) {
+	public CustomRandomPatchFeature(int count, int tries, int spreadHorizontally, int spreadVertically, Holder<PlacedFeature> feature) {
+		this.count = count;
 		this.tries = tries;
 		this.spreadXZ = spreadHorizontally;
 		this.spreadY = spreadVertically;
@@ -38,6 +43,10 @@ public class CustomRandomPatchFeature implements CustomFeature, FeatureRegistrab
 	public PlacedFeature getPlacedFeature() {
 
 		List<PlacementModifier> placementModifiers = new ArrayList<>();
+		placementModifiers.add(CountPlacementModifier.create(this.count));
+		placementModifiers.add(InSquarePlacementModifier.getInstance());
+		placementModifiers.add(PlacedFeatureUtil.WORLD_SURFACE_WG_HEIGHTMAP);
+		placementModifiers.add(BiomePlacementModifier.getInstance());
 
 		return new PlacedFeature(Holder.createDirect(this.getConfiguredFeature()), placementModifiers);
 	}
