1830dee0337af8db310b81bfb655e8a82a8466e0
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-06-24 16:41:56 +0200

    Add CustomSpawnEggItem

---
+++ src/main/java/com/mmodding/mmodding_lib/library/items/CustomSpawnEggItem.java
@@ -0,0 +1,26 @@
+package com.mmodding.mmodding_lib.library.items;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.SpawnEggItem;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CustomSpawnEggItem extends SpawnEggItem implements ItemRegistrable {
+
+    private final AtomicBoolean registered = new AtomicBoolean(false);
+
+    public CustomSpawnEggItem(EntityType<? extends MobEntity> entityType, int primaryColor, int secondaryColor, Settings settings) {
+        super(entityType, primaryColor, secondaryColor, settings);
+    }
+
+    @Override
+    public boolean isNotRegistered() {
+        return !registered.get();
+    }
+
+    @Override
+    public void setRegistered() {
+        registered.set(true);
+    }
+}
