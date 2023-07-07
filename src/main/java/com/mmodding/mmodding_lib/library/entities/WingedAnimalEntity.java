1b119382deb8545f14b66b876ca6d1e8e1332e42
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-07-08 01:48:57 +0200

    Add WingedAnimalEntity

---
+++ src/main/java/com/mmodding/mmodding_lib/library/entities/WingedAnimalEntity.java
@@ -0,0 +1,60 @@
+package com.mmodding.mmodding_lib.library.entities;
+
+import com.google.common.util.concurrent.AtomicDouble;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.world.World;
+
+public abstract class WingedAnimalEntity extends AnimalEntity implements Winged {
+
+	private final AtomicDouble flapProgress = new AtomicDouble();
+	private final AtomicDouble maxWingDeviation = new AtomicDouble();
+	private final AtomicDouble prevMaxWingDeviation = new AtomicDouble();
+	private final AtomicDouble prevFlapProgress = new AtomicDouble();
+	private final AtomicDouble flapSpeed = new AtomicDouble(1.0f);
+	private final AtomicDouble nextFlap = new AtomicDouble(1.0f);
+
+	protected WingedAnimalEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Override
+	public AtomicDouble flapProgress() {
+		return this.flapProgress;
+	}
+
+	@Override
+	public AtomicDouble maxWingDeviation() {
+		return this.maxWingDeviation;
+	}
+
+	@Override
+	public AtomicDouble prevMaxWingDeviation() {
+		return this.prevMaxWingDeviation;
+	}
+
+	@Override
+	public AtomicDouble prevFlapProgress() {
+		return this.prevFlapProgress;
+	}
+
+	@Override
+	public AtomicDouble flapSpeed() {
+		return this.flapSpeed;
+	}
+
+	@Override
+	public AtomicDouble nextFlap() {
+		return this.nextFlap;
+	}
+
+	@Override
+	public boolean hasWings() {
+		return Winged.super.hasWings();
+	}
+
+	@Override
+	public void addFlapEffects() {
+		Winged.super.addFlapEffects();
+	}
+}
