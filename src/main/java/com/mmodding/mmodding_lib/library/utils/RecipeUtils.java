6f0701ad95460841a954f6536ca969e9b3960da6
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-08-05 01:00:24 +0200

    Add RecipeUtils

---
+++ src/main/java/com/mmodding/mmodding_lib/library/utils/RecipeUtils.java
@@ -0,0 +1,26 @@
+package com.mmodding.mmodding_lib.library.utils;
+
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeMatcher;
+import net.minecraft.util.collection.DefaultedList;
+
+public class RecipeUtils {
+
+	public static boolean ingredientMatches(Inventory inventory, Recipe<?> recipe, DefaultedList<Ingredient> ingredients) {
+		RecipeMatcher matcher = new RecipeMatcher();
+		int counter = 0;
+
+		for(int index = 0; index < inventory.size(); index++) {
+			ItemStack stack = inventory.getStack(index);
+			if (!stack.isEmpty()) {
+				counter++;
+				matcher.addInput(stack, 1);
+			}
+		}
+
+		return ingredients.size() == counter && matcher.match(recipe, null);
+	}
+}
