f20cda2e2444f5dad49b37260b81533ecb5479f6
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-08-17 20:01:22 +0200

    Add InventoryUtils

---
+++ src/main/java/com/mmodding/mmodding_lib/library/utils/InventoryUtils.java
@@ -0,0 +1,49 @@
+package com.mmodding.mmodding_lib.library.utils;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.collection.DefaultedList;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InventoryUtils {
+
+    public DefaultedList<ItemStack> sort(DefaultedList<ItemStack> content) {
+        Map<Pair<Item, NbtCompound>, Integer> items = new HashMap<>();
+
+        content.stream().filter(stack -> !stack.isEmpty()).forEach(stack -> {
+            Pair<Item, NbtCompound> item = Pair.of(stack.getItem(), stack.getNbt());
+            int count = items.getOrDefault(item, 0);
+            items.put(item, count + stack.getCount());
+        });
+
+        DefaultedList<ItemStack> sorted = DefaultedList.of();
+
+        items.forEach((item, count) -> {
+            int maxCount = item.getLeft().getMaxCount();
+
+            if (count > maxCount) {
+                int remaining = count;
+
+                do {
+                    int stackCount = remaining - maxCount <= 0 ? remaining : maxCount;
+                    ItemStack stack = new ItemStack(item.getLeft(), stackCount);
+                    stack.setNbt(item.getRight());
+                    sorted.add(stack);
+                    remaining -= maxCount;
+                }
+                while (remaining > 0);
+            }
+            else {
+                ItemStack stack = new ItemStack(item.getLeft(), count);
+                stack.setNbt(item.getRight());
+                sorted.add(stack);
+            }
+        });
+
+        return sorted;
+    }
+}
