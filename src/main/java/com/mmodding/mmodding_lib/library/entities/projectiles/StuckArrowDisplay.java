e1a77a8f3d9e83a7c92accb7afe73b8705cf3140
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-09-24 00:17:19 +0200

    Change StuckArrowDisplay

--- src/main/java/com/mmodding/mmodding_lib/library/entities/projectiles/StuckArrowDisplay.java
+++ src/main/java/com/mmodding/mmodding_lib/library/entities/projectiles/StuckArrowDisplay.java
@@ -1,9 +1,9 @@
 package com.mmodding.mmodding_lib.library.entities.projectiles;
 
-import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.world.World;
 
-public interface StuckArrowDisplay<T extends ArrowEntity> {
+public interface StuckArrowDisplay<T extends PersistentProjectileEntity> {
 
 
 	T getArrowInstance(World world, double x, double y, double z);
--- src/main/java/com/mmodding/mmodding_lib/mixin/injectors/client/StuckArrowsFeatureRendererMixin.java
+++ src/main/java/com/mmodding/mmodding_lib/mixin/injectors/client/StuckArrowsFeatureRendererMixin.java
@@ -1,20 +1,38 @@
 package com.mmodding.mmodding_lib.mixin.injectors.client;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mmodding.mmodding_lib.library.entities.projectiles.StuckArrowDisplay;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.feature.StuckArrowsFeatureRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StuckArrowsFeatureRenderer.class)
 public class StuckArrowsFeatureRendererMixin {
 
-	@ModifyExpressionValue(method = "renderObject", at = @At(value = "NEW", target = "(Lnet/minecraft/world/World;DDD)Lnet/minecraft/entity/projectile/ArrowEntity;"))
-	private ArrowEntity renderObject(ArrowEntity original, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, Entity entity, float directionX, float directionY, float directionZ, float tickDelta) {
-		return entity instanceof StuckArrowDisplay<?> display ? display.getArrowInstance(entity.world, entity.getX(), entity.getY(), entity.getZ()) : original;
+	@Shadow
+	@Final
+	private EntityRenderDispatcher dispatcher;
+
+	@Inject(method = "renderObject", at = @At(value = "HEAD"), cancellable = true)
+	private void renderObject(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, Entity entity, float directionX, float directionY, float directionZ, float tickDelta, CallbackInfo ci) {
+		if (entity instanceof StuckArrowDisplay<?> display) {
+			float f = MathHelper.sqrt(directionX * directionX + directionZ * directionZ);
+			PersistentProjectileEntity persistentProjectileEntity = display.getArrowInstance(entity.world, entity.getX(), entity.getY(), entity.getZ());
+			persistentProjectileEntity.setYaw((float) Math.atan2(directionX, directionZ * 180.0f / Math.PI));
+			persistentProjectileEntity.setPitch((float) Math.atan2(directionY, (f * 180.0f / Math.PI)));
+			persistentProjectileEntity.prevYaw = persistentProjectileEntity.getYaw();
+			persistentProjectileEntity.prevPitch = persistentProjectileEntity.getPitch();
+			this.dispatcher.render(persistentProjectileEntity, 0.0, 0.0, 0.0, 0.0F, tickDelta, matrices, vertexConsumers, light);
+			ci.cancel();
+		}
 	}
 }
