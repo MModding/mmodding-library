5dadadf6e6430eee810ea50d8e95d47c5526a4f0
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-11-15 17:36:40 +0100

    Fix BlockEntityTypeMixin

--- src/main/java/com/mmodding/mmodding_lib/mixin/injectors/BlockEntityTypeMixin.java
+++ src/main/java/com/mmodding/mmodding_lib/mixin/injectors/BlockEntityTypeMixin.java
@@ -2,25 +2,22 @@ package com.mmodding.mmodding_lib.mixin.injectors;
 
 import com.mmodding.mmodding_lib.ducks.BlockEntityTypeDuckInterface;
 import com.mmodding.mmodding_lib.library.blockentities.BlockEntityTypeBlockSupportCallback;
-import com.mojang.datafixers.types.Type;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import org.quiltmc.qsl.base.api.event.Event;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Mixin(BlockEntityType.class)
-public class BlockEntityTypeMixin<T extends BlockEntity> implements BlockEntityTypeDuckInterface {
+public class BlockEntityTypeMixin implements BlockEntityTypeDuckInterface {
 
 	@Unique
 	private final Event<BlockEntityTypeBlockSupportCallback> supportBlocksCallBack = Event.create(BlockEntityTypeBlockSupportCallback.class, callbacks -> supportedBlocks -> {
@@ -29,16 +26,13 @@ public class BlockEntityTypeMixin<T extends BlockEntity> implements BlockEntityT
 		}
 	});
 
-	@Shadow
-	private Set<Block> blocks;
-
-	@Inject(method = "<init>", at = @At("TAIL"))
-	private void init(BlockEntityType.BlockEntityFactory<T> factory, Set<Block> blocks, Type<?> type, CallbackInfo ci) {
-		Set<Block> previousBlocks = new HashSet<>(this.blocks);
-		List<Block> eventBlocks = new ArrayList<>(previousBlocks.stream().toList());
-		this.mmodding_lib$supportBlocksCallback().invoker().onSupportedBlocks(eventBlocks);
-		previousBlocks.addAll(eventBlocks);
-		this.blocks = previousBlocks;
+	@Inject(method = "supports", at = @At("HEAD"), cancellable = true)
+	private void supports(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+		List<Block> supportedBlocks = new ArrayList<>();
+		this.mmodding_lib$supportBlocksCallback().invoker().onSupportedBlocks(supportedBlocks);
+		if (supportedBlocks.contains(state.getBlock())) {
+			cir.setReturnValue(true);
+		}
 	}
 
 	@Override
