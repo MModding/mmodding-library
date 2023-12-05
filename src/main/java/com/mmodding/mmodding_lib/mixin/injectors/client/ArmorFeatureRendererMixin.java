b487d795c365839f5d1142afd3ee3fc1dc5cd4bc
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-12-05 22:19:37 +0100

    Fix ArmorFeatureRendererMixin

--- src/main/java/com/mmodding/mmodding_lib/mixin/injectors/client/ArmorFeatureRendererMixin.java
+++ src/main/java/com/mmodding/mmodding_lib/mixin/injectors/client/ArmorFeatureRendererMixin.java
@@ -2,8 +2,6 @@ package com.mmodding.mmodding_lib.mixin.injectors.client;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mmodding.mmodding_lib.library.glint.client.GlintPack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.render.OverlayTexture;
@@ -20,6 +18,7 @@ import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -27,18 +26,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ArmorFeatureRenderer.class)
 public abstract class ArmorFeatureRendererMixin<T extends LivingEntity, A extends BipedEntityModel<T>> {
 
+	@Unique
+	private ItemStack ref;
+
 	@Shadow
 	protected abstract Identifier getArmorTexture(ArmorItem item, boolean legs, @Nullable String overlay);
 
 	@WrapOperation(method = "renderArmor", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", ordinal = 0))
-	private Item renderArmor(ItemStack stack, Operation<Item> original, @Share("itemStack") LocalRef<ItemStack> ref) {
-		ref.set(stack);
+	private Item renderArmor(ItemStack stack, Operation<Item> original) {
+		this.ref = stack;
 		return original.call(stack);
 	}
 
 	@Inject(method = "renderArmorParts", at = @At(value = "HEAD"), cancellable = true)
-	private void renderArmorParts(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorItem item, boolean usesSecondLayer, A model, boolean legs, float red, float green, float blue, @Nullable String overlay, CallbackInfo ci, @Share("itemStack") LocalRef<ItemStack> ref) {
-		GlintPack.of(ref.get()).ifPresent(glintPack -> {
+	private void renderArmorParts(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorItem item, boolean usesSecondLayer, A model, boolean legs, float red, float green, float blue, @Nullable String overlay, CallbackInfo ci) {
+		GlintPack.of(this.ref).ifPresent(glintPack -> {
 			VertexConsumer vertexConsumer = glintPack.getArmorConsumer(
 				vertexConsumers,
 				RenderLayer.getArmorCutoutNoCull(this.getArmorTexture(item, legs, overlay)),
