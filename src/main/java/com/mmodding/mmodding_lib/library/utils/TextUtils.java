a1fc0ae886dd673c6f45eaf43fd5391f868569cc
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2024-03-17 14:09:32 +0100

    Add TextUtils#drawCenteredTrimmed Update Dependencies

--- src/main/java/com/mmodding/mmodding_lib/library/utils/TextUtils.java
+++ src/main/java/com/mmodding/mmodding_lib/library/utils/TextUtils.java
@@ -1,7 +1,10 @@
 package com.mmodding.mmodding_lib.library.utils;
 
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.StringVisitable;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
 
 public class TextUtils {
 
@@ -10,5 +13,12 @@ public class TextUtils {
 			text.append(" ").append(sibling);
 		}
 		return text;
+	}
+
+	public static void drawCenteredTrimmed(TextRenderer textRenderer, StringVisitable text, int x, int maxWidth, int startY, int startZ, int color) {
+		int lines = textRenderer.wrapLines(text, maxWidth).size();
+		int height = lines * 9;
+		int anchor = MathHelper.abs(startY - startZ);
+		textRenderer.drawTrimmed(text, x, startY + anchor - height / 2, maxWidth, color);
 	}
 }
