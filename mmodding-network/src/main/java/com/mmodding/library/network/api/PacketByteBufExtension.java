b4b3127fac072613a35d8e0782255212946f4cf5
FirstMegaGame4 <firstmegagame4@outlook.fr>
2024-07-16 15:31:19 +0200

    MModding Network Library Java Documentation

--- mmodding-network/src/main/java/com/mmodding/library/network/api/PacketByteBufExtension.java
+++ mmodding-network/src/main/java/com/mmodding/library/network/api/PacketByteBufExtension.java
@@ -8,22 +8,47 @@ import java.util.Optional;
 @InjectedContent(PacketByteBuf.class)
 public interface PacketByteBufExtension {
 
+	/**
+	 * Determines the type of the next handled value, and stores it in an {@link Optional} instance
+	 * if the next value is a handled value, otherwise it returns {@link Optional#empty()}
+	 * @return the next type
+	 */
 	default Optional<Class<?>> peekNextType() {
 		throw new IllegalStateException();
 	}
 
+	/**
+	 * Reads the next handled value.
+	 * @param type the type of the next handled value
+	 * @return the read value
+	 */
 	default <T> T readByHandling(Class<T> type) {
 		throw new IllegalStateException();
 	}
 
+	/**
+	 * Writes a handled value.
+	 * @param type the type of the handled value.
+	 * @param value the written value
+	 */
 	default <T> void writeByHandling(Class<T> type, T value) {
 		throw new IllegalStateException();
 	}
 
+	/**
+	 * Reads the next handled value as an {@link Optional} instance.
+	 * @param type the type of the next handled value
+	 * @return the read value as an optional instance
+	 */
 	default <T> Optional<T> readOptionalByHandling(Class<T> type) {
 		throw new IllegalStateException();
 	}
 
+	/**
+	 * Writes a handled value as an {@link Optional} instance.
+	 * @param type the type of the handled value
+	 * @param value the written value as an optional instance
+	 */
 	default <T> void writeOptionalByHandling(Class<T> type, T value) {
 		throw new IllegalStateException();
 	}
