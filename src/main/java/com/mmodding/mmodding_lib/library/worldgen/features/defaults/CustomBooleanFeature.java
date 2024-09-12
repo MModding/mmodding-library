aee1a8810aeb1244814cddb5f46d4d591bd1765e
FirstMegaGame4 <firstmegagame4@outlook.fr>
2024-09-12 16:29:26 +0200

    Change CustomBooleanFeature

--- src/main/java/com/mmodding/mmodding_lib/library/worldgen/features/defaults/CustomBooleanFeature.java
+++ src/main/java/com/mmodding/mmodding_lib/library/worldgen/features/defaults/CustomBooleanFeature.java
@@ -9,7 +9,6 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.feature.util.PlacedFeatureUtil;
 import org.quiltmc.qsl.worldgen.biome.api.BiomeModifications;
 import org.quiltmc.qsl.worldgen.biome.api.BiomeSelectionContext;
 
@@ -25,18 +24,14 @@ public class CustomBooleanFeature implements CustomFeature, FeatureRegistrable {
 	private final AtomicReference<Identifier> identifier = new AtomicReference<>();
 	private final BiList<PlacedFeature, String> additionalPlacedFeatures = new BiArrayList<>();
 
-	private final Supplier<ConfiguredFeature<?, ?>> firstFeature;
-	private final List<PlacementModifier> firstFeaturePlacementModifiers;
-	private final Supplier<ConfiguredFeature<?, ?>> lastFeature;
-	private final List<PlacementModifier> lastFeaturePlacementModifiers;
+	private final Supplier<CustomFeature> firstFeature;
+	private final Supplier<CustomFeature> lastFeature;
 	private final GenerationStep.Feature step;
 	private final List<PlacementModifier> defaultPlacementModifiers;
 
-	public CustomBooleanFeature(Supplier<ConfiguredFeature<?, ?>> firstFeature, List<PlacementModifier> firstFeaturePlacementModifiers, Supplier<ConfiguredFeature<?, ?>> lastFeature, List<PlacementModifier> lastFeaturePlacementModifiers, GenerationStep.Feature step, PlacementModifier... defaultPlacementModifiers) {
+	public CustomBooleanFeature(Supplier<CustomFeature> firstFeature, Supplier<CustomFeature> lastFeature, GenerationStep.Feature step, PlacementModifier... defaultPlacementModifiers) {
 		this.firstFeature = firstFeature;
-		this.firstFeaturePlacementModifiers = firstFeaturePlacementModifiers;
 		this.lastFeature = lastFeature;
-		this.lastFeaturePlacementModifiers = lastFeaturePlacementModifiers;
 		this.step = step;
 		this.defaultPlacementModifiers = List.of(defaultPlacementModifiers);
 	}
@@ -49,14 +44,8 @@ public class CustomBooleanFeature implements CustomFeature, FeatureRegistrable {
 	@Override
 	public ConfiguredFeature<?, ?> getConfiguredFeature() {
 		return new ConfiguredFeature<>(this.getFeature(), new RandomBooleanFeatureConfig(
-			PlacedFeatureUtil.placedInline(
-				Holder.createDirect(this.firstFeature.get()),
-				this.firstFeaturePlacementModifiers.toArray(PlacementModifier[]::new)
-			),
-			PlacedFeatureUtil.placedInline(
-				Holder.createDirect(this.lastFeature.get()),
-				this.lastFeaturePlacementModifiers.toArray(PlacementModifier[]::new)
-			)
+			Holder.createDirect(this.firstFeature.get().getDefaultPlacedFeature()),
+			Holder.createDirect(this.lastFeature.get().getDefaultPlacedFeature())
 		));
 	}
 
