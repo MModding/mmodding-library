b760294e5cdc4e7ee830704ff3c3f7f3397bd007
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2024-10-25 16:06:06 +0200

    Change LayeredFeature

--- src/main/java/com/mmodding/mmodding_lib/library/worldgen/features/builtin/LayeredFeature.java
+++ src/main/java/com/mmodding/mmodding_lib/library/worldgen/features/builtin/LayeredFeature.java
@@ -4,9 +4,9 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.FeatureConfig;
-import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 
 import java.util.List;
@@ -20,12 +20,12 @@ public class LayeredFeature extends Feature<LayeredFeature.Config> {
 	@Override
 	public boolean place(FeatureContext<Config> context) {
 		context.getConfig().features.forEach(feature -> {
-			PlacedFeature entry = context.getWorld().getRegistryManager().get(Registry.PLACED_FEATURE_KEY).get(feature);
+			ConfiguredFeature<?, ?> entry = context.getWorld().getRegistryManager().get(Registry.CONFIGURED_FEATURE_KEY).get(feature);
 			if (entry != null) {
 				entry.generate(context.getWorld(), context.getGenerator(), context.getRandom(), context.getOrigin());
 			}
 			else {
-				throw new IllegalArgumentException("PlacedFeature at " + feature + "does not exist");
+				throw new IllegalArgumentException("ConfiguredFeature at " + feature + "does not exist");
 			}
 		});
 		return true;
@@ -33,15 +33,15 @@ public class LayeredFeature extends Feature<LayeredFeature.Config> {
 
 	public static class Config implements FeatureConfig {
 
-		private final List<RegistryKey<PlacedFeature>> features;
+		private final List<RegistryKey<ConfiguredFeature<?, ?>>> features;
 
 		public static final Codec<Config> CODEC = RecordCodecBuilder.create(
 			instance -> instance
-				.group(Codec.list(RegistryKey.codec(Registry.PLACED_FEATURE_KEY)).fieldOf("features").forGetter(config -> config.features))
+				.group(Codec.list(RegistryKey.codec(Registry.CONFIGURED_FEATURE_KEY)).fieldOf("features").forGetter(config -> config.features))
 				.apply(instance, Config::new)
 		);
 
-		public Config(List<RegistryKey<PlacedFeature>> features) {
+		public Config(List<RegistryKey<ConfiguredFeature<?, ?>>> features) {
 			this.features = features;
 		}
 	}
