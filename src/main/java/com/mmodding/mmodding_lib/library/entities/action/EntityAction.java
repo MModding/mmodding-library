4efd1dde6f2102727827ac27795ce2d9ec4e1574
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2025-01-30 20:51:41 +0100

    Change EntityAction

--- src/main/java/com/mmodding/mmodding_lib/library/entities/action/EntityAction.java
+++ src/main/java/com/mmodding/mmodding_lib/library/entities/action/EntityAction.java
@@ -6,23 +6,25 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
 
 public class EntityAction {
 
 	private final Entity entity;
 	private final SyncableData<Integer> remainingTime;
-	private final Runnable action;
 	private final int delayBeforeStaring;
 	private final int delayAfterEnding;
 
 	@ClientOnly
 	private final AnimationState state = new AnimationState();
 
-	public EntityAction(Entity entity, Identifier actionName, Runnable actionRunnable, int delayBeforeStarting, int delayAfterEnding) {
+	@Nullable
+	private Runnable action = null;
+
+	public EntityAction(Entity entity, Identifier action, int delayBeforeStarting, int delayAfterEnding) {
 		this.entity = entity;
-		this.remainingTime = new SyncableData<>(entity, actionName, TrackedDataHandlerRegistry.INTEGER);
-		this.action = actionRunnable;
+		this.remainingTime = new SyncableData<>(entity, action, TrackedDataHandlerRegistry.INTEGER);
 		this.delayBeforeStaring = delayBeforeStarting;
 		this.delayAfterEnding = delayAfterEnding;
 	}
@@ -31,8 +33,9 @@ public class EntityAction {
 		return this.remainingTime.get() != -1;
 	}
 
-	public void execute() {
+	public void execute(Runnable action) {
 		if (this.entity.getWorld() instanceof ServerWorld) {
+			this.action = action;
 			this.remainingTime.set(0);
 			this.remainingTime.synchronize();
 		}
@@ -53,7 +56,9 @@ public class EntityAction {
 	private void serverTick() {
 		if (this.remainingTime.get() >= 0 && this.remainingTime.get() <= this.delayBeforeStaring + this.delayAfterEnding) {
 			if (this.remainingTime.get() == this.delayBeforeStaring) {
+				assert this.action != null;
 				this.action.run();
+				this.action = null;
 			}
 			this.remainingTime.set(this.remainingTime.get() + 1);
 			if (this.remainingTime.get() % 200 == 0) {
