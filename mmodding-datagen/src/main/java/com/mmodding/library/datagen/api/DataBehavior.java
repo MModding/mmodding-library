63189fe99a11f6f50d5f7bdcfc1e3e137876e76d
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2025-03-15 20:36:56 +0100

    Change DataBehavior

--- mmodding-datagen/src/main/java/com/mmodding/library/datagen/api/DataBehavior.java
+++ mmodding-datagen/src/main/java/com/mmodding/library/datagen/api/DataBehavior.java
@@ -1,18 +1,25 @@
 package com.mmodding.library.datagen.api;
 
 import com.mmodding.library.datagen.api.lang.LangProcessor;
+import com.mmodding.library.datagen.api.loot.block.BlockLootProcessor;
 
 public interface DataBehavior {
 
 	/**
-	 * States the mod namespace
+	 * Refers to the mod namespace.
  	 * @return the mod namespace
 	 */
 	String namespace();
 
 	/**
-	 * States the Default Lang Processor that the elements should use
+	 * States the Default Lang Processor that the elements should use.
  	 * @return the Default Lang Processor
 	 */
 	LangProcessor<?> getLangProcessor();
+
+	/**
+	 * States the Default Block Loot Processor that blocks should use.
+	 * @return the Default Block Loot Processor
+	 */
+	BlockLootProcessor getBlockLootProcessor();
 }
