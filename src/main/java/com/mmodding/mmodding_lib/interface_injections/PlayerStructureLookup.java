43dcc9a54c51a2d08261512f46b6b0e31c84c229
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2025-03-18 22:11:59 +0100

    Add PlayerStructureLookup

---
+++ src/main/java/com/mmodding/mmodding_lib/interface_injections/PlayerStructureLookup.java
@@ -0,0 +1,29 @@
+package com.mmodding.mmodding_lib.interface_injections;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.feature.StructureFeature;
+import org.quiltmc.qsl.base.api.util.InjectedInterface;
+
+import java.util.Set;
+
+@InjectedInterface(PlayerEntity.class)
+public interface PlayerStructureLookup {
+
+	/**
+	 * This method returns a set of registry keys of structures the player is in or that are aside from it.
+	 * @return the set of registry keys of structures
+	 */
+	default Set<RegistryKey<StructureFeature>> getClosestStructures() {
+		throw new IllegalStateException();
+	}
+
+	/**
+	 * This method checks if a specific structure is close to the player.
+	 * @param structure the structure
+	 * @return a boolean specifying if the structure is close or not
+	 */
+	default boolean isCloseToStructure(RegistryKey<StructureFeature> structure) {
+		return this.getClosestStructures().contains(structure);
+	}
+}
--- src/main/java/com/mmodding/mmodding_lib/mixin/injectors/PlayerEntityMixin.java
+++ src/main/java/com/mmodding/mmodding_lib/mixin/injectors/PlayerEntityMixin.java
@@ -2,17 +2,26 @@ package com.mmodding.mmodding_lib.mixin.injectors;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mmodding.mmodding_lib.ducks.PlayerEntityDuckInterface;
+import com.mmodding.mmodding_lib.interface_injections.PlayerStructureLookup;
 import com.mmodding.mmodding_lib.interface_injections.SoundtrackPlayerContainer;
+import com.mmodding.mmodding_lib.library.entities.data.MModdingTrackedDataHandlers;
+import com.mmodding.mmodding_lib.library.entities.data.syncable.SyncableData;
 import com.mmodding.mmodding_lib.library.items.CustomBowItem;
 import com.mmodding.mmodding_lib.library.items.settings.AdvancedItemSettings;
 import com.mmodding.mmodding_lib.library.items.settings.ItemDropped;
 import com.mmodding.mmodding_lib.library.soundtracks.SoundtrackPlayer;
+import com.mmodding.mmodding_lib.library.utils.MModdingIdentifier;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.feature.StructureFeature;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -22,11 +31,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends EntityMixin implements PlayerEntityDuckInterface, SoundtrackPlayerContainer {
+public abstract class PlayerEntityMixin extends EntityMixin implements PlayerEntityDuckInterface, PlayerStructureLookup, SoundtrackPlayerContainer {
+
+	@Unique
+	private final SyncableData<List<Identifier>> closestStructures = new SyncableData<>(
+		List.of(),
+		(PlayerEntity) (Object) this,
+		new MModdingIdentifier("closest_structures"),
+		MModdingTrackedDataHandlers.IDENTIFIER_LIST
+	);
+
+	@Unique
+	private int structureUpdateCooldown = 0;
 
 	@Unique
 	private boolean invincible;
+
+	@Inject(method = "tick", at = @At("TAIL"))
+	private void updateCurrentStructure(CallbackInfo ci) {
+		if (this.getWorld() instanceof ServerWorld serverWorld) {
+			this.structureUpdateCooldown++;
+			if (this.structureUpdateCooldown >= 100) {
+				this.structureUpdateCooldown = 0;
+				Set<StructureFeature> structureFeature = serverWorld.getStructureManager().getStructuresAt(this.getBlockPos()).keySet();
+				Registry<StructureFeature> structureFeatureRegistry = serverWorld.getRegistryManager().get(Registry.STRUCTURE_WORLDGEN);
+				this.closestStructures.set(structureFeature.stream().map(structureFeatureRegistry::getId).toList());
+				this.closestStructures.synchronize();
+			}
+		}
+	}
 
 	@Inject(method = "dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
 	private void dropItem(ItemStack stack, boolean throwRandomly, boolean retainOwnership, CallbackInfoReturnable<ItemEntity> cir, double d, ItemEntity itemEntity) {
@@ -58,6 +96,15 @@ public abstract class PlayerEntityMixin extends EntityMixin implements PlayerEnt
 		if (this.mmodding_lib$isInvincible()) {
 			cir.setReturnValue(false);
 		}
+	}
+
+	@Override
+	@SuppressWarnings("AddedMixinMembersNamePattern")
+	public Set<RegistryKey<StructureFeature>> getClosestStructures() {
+		return this.closestStructures.get()
+			.stream()
+			.map(identifier -> RegistryKey.of(Registry.STRUCTURE_WORLDGEN, identifier))
+			.collect(Collectors.toSet());
 	}
 
 	@Override
