ea7b5909b8ac3f73fcf2ccc7d3e2c773eea359dc
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2025-05-09 22:39:10 +0200

    Fix WorldListWidgetEntryMixin

--- src/main/java/com/mmodding/mmodding_lib/mixin/injectors/client/WorldListWidgetEntryMixin.java
+++ src/main/java/com/mmodding/mmodding_lib/mixin/injectors/client/WorldListWidgetEntryMixin.java
@@ -2,7 +2,7 @@ package com.mmodding.mmodding_lib.mixin.injectors.client;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.mmodding.mmodding_lib.MModdingLib;
+import com.mmodding.mmodding_lib.client.MModdingLibClient;
 import net.minecraft.client.gui.screen.world.WorldListWidget;
 import net.minecraft.world.level.storage.LevelSummary;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +13,7 @@ public class WorldListWidgetEntryMixin {
 
 	@WrapOperation(method = "play", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/LevelSummary$ConversionWarning;promptsBackup()Z"))
 	private boolean preventBackupConditionally(LevelSummary.ConversionWarning instance, Operation<Boolean> original) {
-		if (!MModdingLib.LIBRARY_CONFIG.getContent().getBoolean("showExperimentalSettingsScreen")) {
+		if (!MModdingLibClient.LIBRARY_CLIENT_CONFIG.getContent().getBoolean("showExperimentalSettingsScreen")) {
 			return false;
 		}
 		else {
