e4bd05d57078c7aaa04999d0a5ef5548b0b070cf
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-03-18 19:57:34 +0100

    Boom!

--- mmodding-datagen/src/main/java/com/mmodding/library/datagen/impl/management/handler/BlockFamilyTypeImpl.java
+++ mmodding-datagen/src/main/java/com/mmodding/library/datagen/impl/management/handler/BlockFamilyTypeImpl.java
@@ -1,17 +1,24 @@
 package com.mmodding.library.datagen.impl.management.handler;
 
+import com.mmodding.library.core.api.registry.IdentifierUtil;
 import com.mmodding.library.datagen.api.family.BlockFamilyProcessor;
+import com.mmodding.library.datagen.api.lang.DefaultLangProcessors;
+import com.mmodding.library.datagen.api.lang.TranslationProcessor;
 import com.mmodding.library.datagen.api.management.DataContentType;
 import com.mmodding.library.java.api.either.Either;
 import com.mmodding.library.java.api.list.BiList;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Block;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.family.BlockFamily;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -20,8 +27,36 @@ public class BlockFamilyTypeImpl implements DataContentType<BlockFamily, BlockFa
 
 	@Override
 	public void handleContent(FabricDataGenerator.Pack pack, BiList<BlockFamilyProcessor, List<BlockFamily>> contentToProcess) {
+		pack.addProvider((output, lookup) -> new AutomatedBlockFamilyTranslations(output, contentToProcess));
 		pack.addProvider((output, lookup) -> new AutomatedBlockFamilyModels(output, contentToProcess));
 		pack.addProvider((output, lookup) -> new AutomatedBlockFamilyRecipes(output, contentToProcess));
+	}
+
+	private static class AutomatedBlockFamilyTranslations extends FabricLanguageProvider {
+
+		private final BiList<BlockFamilyProcessor, List<BlockFamily>> contentToProcess;
+
+		public AutomatedBlockFamilyTranslations(FabricDataOutput output, BiList<BlockFamilyProcessor, List<BlockFamily>> contentToProcess) {
+			super(output);
+			this.contentToProcess = contentToProcess;
+		}
+
+		@Override
+		public void generateTranslations(TranslationBuilder translationBuilder) {
+			TranslationProcessor<Block> classicProcessor = DefaultLangProcessors.getClassic(); // I don't think that it really needs to be customized here. Block Families are meant to naming conventions.
+			this.contentToProcess.forEach((processor, families) -> {
+				families.forEach(family -> {
+					RegistryKey<Block> mainKey = Registries.BLOCK.getKey(family.getBaseBlock()).orElseThrow();
+					translationBuilder.add(family.getBaseBlock(), classicProcessor.process(mainKey));
+					family.getVariants().forEach((variant, block) -> translationBuilder.add(block, classicProcessor.process(mainKey.mapValue(identifier -> IdentifierUtil.extend(identifier, variant.getName())))));
+				});
+			});
+		}
+
+		@Override
+		public String getName() {
+			return "Automated Block Family " + super.getName();
+		}
 	}
 
 	private static class AutomatedBlockFamilyModels extends FabricModelProvider {
