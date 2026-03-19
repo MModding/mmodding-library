cd2d36ff78bcbbd09164cca904d00b8e037e9a49
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-03-19 09:57:04 +0100

    Datagen Refactor the Sequel: Road to Madness

--- mmodding-datagen/src/main/java/com/mmodding/library/datagen/impl/management/ClassContentOperator.java
+++ mmodding-datagen/src/main/java/com/mmodding/library/datagen/impl/management/ClassContentOperator.java
@@ -1,63 +1,11 @@
 package com.mmodding.library.datagen.impl.management;
 
-import com.mmodding.library.datagen.api.management.DataContentType;
-import com.mmodding.library.datagen.api.management.resolver.DataContentResolver;
-import com.mmodding.library.java.api.container.Pair;
-import com.mmodding.library.java.api.list.BiList;
-import com.mmodding.library.java.api.list.TriList;
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 public class ClassContentOperator {
-
-	private final List<?> elements;
-	// list of (pair of (handler, type), filter, processor)
-	private final TriList<Pair<DataContentType<?, ?>, Class<?>>, Predicate<?>, Object> operations;
-
-	public ClassContentOperator(Class<?> sourceClass) {
-		this.elements = ClassContentOperator.extract(sourceClass);
-		this.operations = TriList.create();
-	}
-
-	public <T, P> void add(DataContentType<T, P> handler, Class<T> type, Predicate<T> filter, P processor) {
-		this.operations.add(Pair.create(handler, type), filter, processor);
-	}
-
-	@SuppressWarnings({"unchecked", "rawtypes"})
-	public void operate(FabricDataGenerator.Pack pack) {
-		// dynamic retention of resolved elements of a type
-		Map<Class<?>, List<?>> collected = new Object2ObjectOpenHashMap<>();
-		Map<DataContentType<?, Object>, BiList<Object, List<?>>> tasks = new Object2ObjectOpenHashMap<>();
-		this.operations.forEach((pair, filter, processor) -> {
-			if (!collected.containsKey(pair.second())) {
-				List<?> targets = this.elements.stream()
-					.filter(
-						element -> pair.second().isAssignableFrom(element.getClass()) ||
-							(DataContentResolverImpl.REGISTRY.containsKey(element.getClass())
-							&& pair.second().isAssignableFrom(DataContentResolverImpl.REGISTRY.getFirstValue(element.getClass())))
-					)
-					.flatMap(
-						element -> pair.second().isAssignableFrom(element.getClass())
-							? Stream.of(element)
-							: ((DataContentResolver<Object, ?>) DataContentResolverImpl.REGISTRY.getSecondValue(element.getClass())).resolve(element).stream()
-					)
-					.toList();
-				collected.put(pair.second(), targets);
-			}
-			List<?> typeCollected = collected.get(pair.second());
-			List<?> filtered = typeCollected.stream().filter((Predicate<Object>) filter).toList();
-			tasks.computeIfAbsent((DataContentType<?, Object>) pair.first(), ignored -> BiList.create()).add(processor, filtered);
-		});
-		tasks.forEach((handler, contentToProcess) -> handler.handleContent(pack, (BiList) contentToProcess));
-	}
 
 	public static List<?> extract(Class<?> sourceClass) {
 		return Arrays.stream(sourceClass.getDeclaredFields())
--- mmodding-datagen/src/main/java/com/mmodding/library/datagen/impl/management/DataManagerImpl.java
+++ mmodding-datagen/src/main/java/com/mmodding/library/datagen/impl/management/DataManagerImpl.java
@@ -2,31 +2,75 @@ package com.mmodding.library.datagen.impl.management;
 
 import com.mmodding.library.datagen.api.management.DataManager;
 import com.mmodding.library.datagen.api.management.DataContentType;
+import com.mmodding.library.datagen.api.management.resolver.DataContentResolver;
+import com.mmodding.library.java.api.container.Pair;
+import com.mmodding.library.java.api.list.BiList;
+import com.mmodding.library.java.api.list.TriList;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class DataManagerImpl implements DataManager {
 
-	private final Map<Class<?>, ClassContentOperator> operators = new Object2ObjectOpenHashMap<>();
+	// dynamic approach
+	private final Map<Class<?>, List<?>> rawElements = new Object2ObjectOpenHashMap<>();
+	private final Map<Class<?>, Map<Class<?>, List<?>>> rawTypedElements = new Object2ObjectOpenHashMap<>();
+	// map of (handler, triple list of (sourceClass, pair of (type, filter), processor))
+	private final Map<DataContentType<?, Object>, TriList<Class<?>, Pair<Class<?>, Predicate<?>>, Object>> dataCoverage = new Object2ObjectOpenHashMap<>();
 
+	@SuppressWarnings({"unchecked", "rawtypes"})
 	public void loadElements(FabricDataGenerator.Pack pack) {
-		this.operators.values().forEach(operator -> operator.operate(pack));
+		this.dataCoverage.forEach((handler, tasks) -> {
+			BiList<Object, List<?>> contentToProcess = BiList.create();
+			tasks.forEach((sourceClass, pair, processor) -> {
+				Map<Class<?>, List<?>> typeFiltered = this.rawTypedElements.computeIfAbsent(sourceClass, ignored -> new Object2ObjectOpenHashMap<>());
+				if (!typeFiltered.containsKey(pair.first())) {
+					typeFiltered.put(
+						pair.first(),
+						this.rawElements.get(sourceClass)
+							.stream()
+							.filter(
+								element -> pair.first().isAssignableFrom(element.getClass()) ||
+									(DataContentResolverImpl.REGISTRY.containsKey(element.getClass())
+										&& pair.first().isAssignableFrom(DataContentResolverImpl.REGISTRY.getFirstValue(element.getClass())))
+							)
+							.flatMap(
+								element -> pair.first().isAssignableFrom(element.getClass())
+									? Stream.of(element)
+									: ((DataContentResolver<Object, ?>) DataContentResolverImpl.REGISTRY.getSecondValue(element.getClass())).resolve(element).stream()
+							)
+							.toList()
+					);
+				}
+				contentToProcess.add(processor, typeFiltered.get(pair.first()).stream().filter((Predicate) pair.second()).toList());
+			});
+			handler.handleContent(pack, (BiList) contentToProcess);
+		});
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public <T, P> DataManager data(Class<?> sourceClass, Class<T> type, DataContentType<T, P> handler, P processor) {
-		ClassContentOperator operator = this.operators.computeIfAbsent(sourceClass, ClassContentOperator::new);
-		operator.add(handler, type, ignored -> true, processor);
+		if (!this.rawElements.containsKey(sourceClass)) {
+			this.rawElements.put(sourceClass, ClassContentOperator.extract(sourceClass));
+		}
+		this.dataCoverage.computeIfAbsent((DataContentType<?, Object>) handler, ignored -> TriList.create())
+				.add(sourceClass, Pair.create(type, ignored -> true), processor);
 		return this;
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public <T, P> DataManager data(Class<?> sourceClass, Class<T> type, Predicate<T> filter, DataContentType<T, P> handler, P processor) {
-		ClassContentOperator operator = this.operators.computeIfAbsent(sourceClass, ClassContentOperator::new);
-		operator.add(handler, type, filter, processor);
+		if (!this.rawElements.containsKey(sourceClass)) {
+			this.rawElements.put(sourceClass, ClassContentOperator.extract(sourceClass));
+		}
+		this.dataCoverage.computeIfAbsent((DataContentType<?, Object>) handler, ignored -> TriList.create())
+				.add(sourceClass, Pair.create(type, filter), processor);
 		return this;
 	}
 }
