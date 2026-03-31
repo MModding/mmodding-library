04b4c30376663f9115cf8ec850e3f10631188fff
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-03-31 08:45:46 +0200

    Add AreaUtil

---
+++ mmodding-math/src/main/java/com/mmodding/library/math/api/AreaUtil.java
@@ -0,0 +1,65 @@
+package com.mmodding.library.math.api;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+
+// Will need to hide that to impl later.
+public class AreaUtil {
+
+	public static void forBlockPosInLine(BlockPos pos1, BlockPos pos2, Consumer<? super BlockPos> consumer) {
+		Vec3d vector = Vec3d.ofCenter(pos1).relativize(Vec3d.ofCenter(pos2)).normalize();
+		Vec3d mutable = Vec3d.ofCenter(pos1);
+		do {
+			mutable = mutable.add(vector);
+			consumer.accept(BlockPos.ofFloored(mutable));
+		} while (!BlockPos.ofFloored(mutable).equals(pos2));
+	}
+
+	public static void forBlockPosInCubicRadius(BlockPos pos, int radius, Consumer<? super BlockPos> execute) {
+		BlockPos.iterate(
+			pos.getX() - radius,
+			pos.getY() - radius,
+			pos.getZ() - radius,
+			pos.getX() + radius,
+			pos.getY() + radius,
+			pos.getZ() + radius
+		).forEach(execute);
+	}
+
+	public static void iterateFromCenterInSquare(BlockPos center, int radius, Consumer<? super BlockPos> action) {
+		AreaUtil.iterateFromCenter(center, radius, radius, radius, action);
+	}
+
+	public static void iterateFromCenter(BlockPos center, int radiusX, int radiusY, int radiusZ, Consumer<? super BlockPos> action) {
+		Set<BlockPos> alreadyChecked = new HashSet<>();
+		BlockPos firstExtremis = center.add(radiusX, radiusY, radiusZ);
+		BlockPos lastExtremis = center.add(-radiusX, -radiusY, -radiusZ);
+		for (Direction direction : Direction.values()) {
+			AreaUtil.checkActionNext(center.offset(direction), direction.getOpposite(), alreadyChecked, firstExtremis, lastExtremis, action);
+		}
+	}
+
+	private static void checkActionNext(BlockPos pos, Direction excluded, Set<BlockPos> alreadyChecked, BlockPos firstExtremis, BlockPos lastExtremis, Consumer<? super BlockPos> action) {
+		if (!alreadyChecked.contains(pos)) {
+			BlockPos firstSubtract = pos.subtract(firstExtremis);
+			BlockPos lastSubtract = pos.subtract(lastExtremis);
+			boolean firstCheck = firstSubtract.getX() <= 0 && firstSubtract.getY() <= 0 && firstSubtract.getZ() <= 0;
+			boolean lastCheck = -lastSubtract.getX() <= 0 && -lastSubtract.getY() <= 0 && -lastSubtract.getZ() <= 0;
+
+			if (firstCheck && lastCheck) {
+				alreadyChecked.add(pos);
+				action.accept(pos);
+				for (Direction direction : Direction.values()) {
+					if (direction != excluded) {
+						AreaUtil.checkActionNext(pos.offset(direction), direction.getOpposite(), alreadyChecked, firstExtremis, lastExtremis, action);
+					}
+				}
+			}
+		}
+	}
+}
