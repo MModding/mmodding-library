a72fc1b72ba385636b5dafe1af35166cf28ec0e8
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-04-20 19:30:47 +0200

    Fix DoubleCropBlock (yeah)

---
+++ mmodding-block/src/main/java/com/mmodding/library/block/mixin/DoublePlantBlockMixin.java
@@ -0,0 +1,20 @@
+package com.mmodding.library.block.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.ScheduledTickAccess;
+import net.minecraft.world.level.block.DoublePlantBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(DoublePlantBlock.class)
+public class DoublePlantBlockMixin {
+
+	@Shadow
+	protected BlockState updateShape(BlockState state, LevelReader level, ScheduledTickAccess ticks, BlockPos pos, Direction directionToNeighbour, BlockPos neighbourPos, BlockState neighbourState, RandomSource random) {
+		throw new IllegalStateException();
+	}
+}
--- mmodding-block/src/main/java/com/mmodding/library/block/mixin/PitcherCropBlockMixin.java
+++ mmodding-block/src/main/java/com/mmodding/library/block/mixin/PitcherCropBlockMixin.java
@@ -3,6 +3,11 @@ package com.mmodding.library.block.mixin;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.mmodding.library.block.api.catalog.DoubleCropBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.ScheduledTickAccess;
 import net.minecraft.world.level.block.PitcherCropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -12,10 +17,20 @@ import org.spongepowered.asm.mixin.Mixin;
 import java.util.function.Function;
 
 @Mixin(PitcherCropBlock.class)
-public class PitcherCropBlockMixin {
+public class PitcherCropBlockMixin extends DoublePlantBlockMixin {
 
 	@WrapMethod(method = "makeShapes")
 	public Function<BlockState, VoxelShape> cancelIfCustom(Operation<Function<BlockState, VoxelShape>> original) {
 		return (((Object) this) instanceof DoubleCropBlock) ? _ -> Shapes.empty() : original.call();
+	}
+
+	@WrapMethod(method = "updateShape")
+	public BlockState skipSuperCallIfCustom(BlockState state, LevelReader level, ScheduledTickAccess ticks, BlockPos pos, Direction directionToNeighbour, BlockPos neighbourPos, BlockState neighbourState, RandomSource random, Operation<BlockState> original) {
+		if (((Object) this) instanceof DoubleCropBlock) {
+			return super.updateShape(state, level, ticks, pos, directionToNeighbour, neighbourPos, neighbourState, random);
+		}
+		else {
+			return original.call(state, level, ticks, pos, directionToNeighbour, neighbourPos, neighbourState, random);
+		}
 	}
 }
