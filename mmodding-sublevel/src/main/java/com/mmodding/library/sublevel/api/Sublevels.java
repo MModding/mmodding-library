cab5980090c538dfb86e03535f002d905644a71a
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-05-07 22:15:09 +0200

    Change Sublevels

--- mmodding-sublevel/src/main/java/com/mmodding/library/sublevel/api/Sublevels.java
+++ mmodding-sublevel/src/main/java/com/mmodding/library/sublevel/api/Sublevels.java
@@ -7,11 +7,11 @@ import net.minecraft.world.level.dimension.LevelStem;
 
 public class Sublevels {
 
-	public static SublevelType<ChunkPos> createPositionAttachedType(ResourceKey<LevelStem> stem, int sideChunksAmount) {
-		return SublevelType.create(pos -> "chunk-" + pos.x() + "-" + pos.z(), stem, sideChunksAmount);
+	public static SublevelType<ChunkPos> createPositionAttachedType(ResourceKey<LevelStem> stem, int chunkSquareRadius) {
+		return SublevelType.create(pos -> "chunk_" + pos.x() + "-" + pos.z(), stem, chunkSquareRadius);
 	}
 
-	public static SublevelType<Player> createPlayerAttachedType(ResourceKey<LevelStem> stem, int sideChunksAmount) {
-		return SublevelType.create(player -> "player-" + player.getStringUUID(), stem, sideChunksAmount);
+	public static SublevelType<Player> createPlayerAttachedType(ResourceKey<LevelStem> stem, int chunkSquareRadius) {
+		return SublevelType.create(player -> "player_" + player.getStringUUID(), stem, chunkSquareRadius);
 	}
 }
