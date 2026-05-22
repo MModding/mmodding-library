bd0b3c739017e426f40a824ca85544d67eebb3f3
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-05-22 09:27:10 +0200

    Update Modules Table

--- mmodding-task/src/main/java/com/mmodding/library/task/impl/PersistentTaskRegistryImpl.java
+++ mmodding-task/src/main/java/com/mmodding/library/task/impl/PersistentTaskRegistryImpl.java
@@ -5,6 +5,8 @@ import com.mmodding.library.task.api.Task;
 import com.mojang.serialization.Codec;
 import net.minecraft.resources.Identifier;
 
+import java.util.Objects;
+
 public class PersistentTaskRegistryImpl {
 
 	public static final LiteRegistry<Codec<? extends Task>> CODECS = LiteRegistry.create();
@@ -13,5 +15,13 @@ public class PersistentTaskRegistryImpl {
 
 	public static <T extends Task> void register(Identifier identifier, Codec<T> contextCodec) {
 		CODECS.register(identifier, contextCodec);
+	}
+
+	public static Codec<? extends Task> get(Identifier identifier) {
+		return Objects.requireNonNull(CODECS.get(identifier), "Unregistered Persistent Task Context Codec for " + identifier);
+	}
+
+	public static Identifier getCodecId(Task task) {
+		return Objects.requireNonNull(CODECS.getId(task.codec()), "Unregistered Persistent Task Context Codec for class " + task.getClass());
 	}
 }
--- mmodding-task/src/main/java/com/mmodding/library/task/impl/TaskHolder.java
+++ mmodding-task/src/main/java/com/mmodding/library/task/impl/TaskHolder.java
@@ -61,7 +61,7 @@ public class TaskHolder {
 			if (parsedMapResult.isSuccess()) {
 				MapLike<T> mapLike = parsedMapResult.getOrThrow();
 				Identifier type = Identifier.parse(ops.getStringValue(mapLike.get("type")).getOrThrow());
-				Codec<? extends Task> codec = PersistentTaskRegistryImpl.CODECS.get(type);
+				Codec<? extends Task> codec = PersistentTaskRegistryImpl.get(type);
 				int delay = ops.getNumberValue(mapLike.get("delay")).getOrThrow().intValue();
 				int step = ops.getNumberValue(Objects.requireNonNullElse(mapLike.get("step"), ops.createInt(0))).getOrThrow().intValue();
 				return DataResult.success(Pair.of(new TaskHolder(codec.decode(ops, mapLike.get("context")).getOrThrow().getFirst(), delay, step), input));
@@ -74,7 +74,7 @@ public class TaskHolder {
 		@Override
 		public <T> DataResult<T> encode(TaskHolder input, DynamicOps<T> ops, T prefix) {
 			T map = ops.createMap(new Object2ObjectOpenHashMap<>());
-			map = ops.set(map, "type", ops.createString(PersistentTaskRegistryImpl.CODECS.getId(input.task.codec()).toString()));
+			map = ops.set(map, "type", ops.createString(PersistentTaskRegistryImpl.getCodecId(input.task).toString()));
 			map = ops.set(map, "delay", ops.createInt(input.delay));
 			if (input.task instanceof MultiStepTask) {
 				map = ops.set(map, "step", ops.createInt(input.step));
