d11b32891a2e1bac75e32e8b5722d560ffb29633
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-05-26 17:57:35 +0200

    Remove MModdingItemProperties#decoratedPotPattern

--- mmodding-item/src/main/java/com/mmodding/library/item/api/properties/MModdingItemProperties.java
+++ mmodding-item/src/main/java/com/mmodding/library/item/api/properties/MModdingItemProperties.java
@@ -2,29 +2,14 @@ package com.mmodding.library.item.api.properties;
 
 import com.mmodding.library.core.api.MModdingLibrary;
 import com.mmodding.library.core.api.management.info.InjectedContent;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.entity.DecoratedPotPattern;
 
 import java.util.List;
-import java.util.function.Function;
 
 @InjectedContent(Item.Properties.class)
 public interface MModdingItemProperties {
 
-	CustomItemProperty<ResourceKey<DecoratedPotPattern>> DECORATED_POT_PATTERN = CustomItemProperty.create(MModdingLibrary.createId("decorated_pot_pattern"), ResourceKey.class, null);
-
-
 	CustomItemProperty<List<String>> TRINKET_SLOTS = CustomItemProperty.create(MModdingLibrary.createId("trinket_slots"), List.class, null);
-
-	/**
-	 * Allows setting a decorated pot pattern asset for this item.
-	 * @param pattern the decorated pot pattern
-	 * @return the current item properties
-	 */
-	default Item.Properties decoratedPotPattern(ResourceKey<DecoratedPotPattern> pattern) {
-		return this.custom(DECORATED_POT_PATTERN, pattern);
-	}
 
 	/**
 	 * Allows setting that the current item can also act as a trinket.
