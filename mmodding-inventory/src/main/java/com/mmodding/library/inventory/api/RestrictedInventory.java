212a6802809d4c31914ff7ab4078ef406554a274
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-06-18 15:37:21 +0200

    Breaking Changes on MModding Inventory Module (it might also get renamed to mmodding-container in the future, we'll see what needs to be done with it)

--- mmodding-inventory/src/main/java/com/mmodding/library/inventory/api/RestrictedInventory.java
+++ mmodding-inventory/src/main/java/com/mmodding/library/inventory/api/RestrictedInventory.java
@@ -1,48 +1,31 @@
 package com.mmodding.library.inventory.api;
 
 import java.util.function.Predicate;
-import net.minecraft.core.NonNullList;
+
 import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 /**
  * An implementation of {@link Container} which allows restricting its usage by players with predicates.
  */
-public interface RestrictedInventory extends BasicInventory {
+public abstract class RestrictedInventory extends SimpleContainer {
 
-	Predicate<Player> playerUsePredicate();
+	private final Predicate<Player> playerUsePredicate;
 
-	static RestrictedInventory of(NonNullList<ItemStack> content, Predicate<Player> playerUsePredicate) {
-
-		return new RestrictedInventory() {
-
-			@Override
-			public NonNullList<ItemStack> getContent() {
-				return content;
-			}
-
-			@Override
-			public Predicate<Player> playerUsePredicate() {
-				return playerUsePredicate;
-			}
-		};
+	public RestrictedInventory(Predicate<Player> playerUsePredicate, int size) {
+		super(size);
+		this.playerUsePredicate = playerUsePredicate;
 	}
 
-	static RestrictedInventory ofSize(int size, Predicate<Player> playerUsePredicate) {
-		return RestrictedInventory.of(NonNullList.withSize(size, ItemStack.EMPTY), playerUsePredicate);
-	}
-
-	/**
-	 * Returns a new instance of the current inventory without its restrictions.
-	 * @return a new basic inventory
-	 */
-	default BasicInventory avoid() {
-		return BasicInventory.of(this.getContent());
+	public RestrictedInventory(Predicate<Player> playerUsePredicate, ItemStack... stacks) {
+		super(stacks);
+		this.playerUsePredicate = playerUsePredicate;
 	}
 
 	@Override
-	default boolean stillValid(Player player) {
-		return this.playerUsePredicate().test(player);
+	public boolean stillValid(Player player) {
+		return this.playerUsePredicate.test(player);
 	}
 }
