9edbca20539e4326c03d935e0977819792a70a45
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2026-07-03 12:00:03 +0200

    Apply IdentityCompanion

--- mmodding-item/src/main/java/com/mmodding/library/item/mixin/ItemPropertiesMixin.java
+++ mmodding-item/src/main/java/com/mmodding/library/item/mixin/ItemPropertiesMixin.java
@@ -3,44 +3,31 @@ package com.mmodding.library.item.mixin;
 import com.mmodding.library.item.api.properties.CustomItemProperty;
 import com.mmodding.library.item.api.properties.MModdingItemProperties;
 import com.mmodding.library.item.impl.property.CustomItemPropertyImpl;
+import com.mmodding.library.item.impl.property.ItemPropertiesDuck;
 import com.mmodding.library.java.api.list.BiList;
 import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
-import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.Properties.class)
-public class ItemPropertiesMixin implements MModdingItemProperties {
-
-	@Shadow
-	private @Nullable ResourceKey<Item> id;
+public class ItemPropertiesMixin implements MModdingItemProperties, ItemPropertiesDuck {
 
 	@Unique
 	private final BiList<Identifier, Object> customItemPropertiesQueue = BiList.create();
 
-	@Inject(method = "setId", at = @At("TAIL"))
-	private void dequeueAll(ResourceKey<Item> id, CallbackInfoReturnable<Item.Properties> cir) {
-		this.customItemPropertiesQueue.forEach((property, value) ->
-			CustomItemPropertyImpl.REGISTRY.getOrCreateCompanion(this.id).register(property, value)
-		);
-		this.customItemPropertiesQueue.clear();
-	}
-
 	@Override
 	@SuppressWarnings("AddedMixinMembersNamePattern")
 	public <T> Item.Properties custom(CustomItemProperty<T> property, T value) {
-		if (this.id == null) {
-			this.customItemPropertiesQueue.add(property.getIdentifier(), value);
-		}
-		else {
-			CustomItemPropertyImpl.REGISTRY.getOrCreateCompanion(this.id).register(property.getIdentifier(), value);
-		}
+		this.customItemPropertiesQueue.add(property.getIdentifier(), value);
 		return (Item.Properties) (Object) this;
+	}
+
+	@Override
+	public void mmodding$applyCustomProperties(Item item) {
+		this.customItemPropertiesQueue.forEach((property, value) ->
+			CustomItemPropertyImpl.PROPERTIES_COMPANION.getOrCreateCompanion(item).register(property, value)
+		);
+		this.customItemPropertiesQueue.clear();
 	}
 }
