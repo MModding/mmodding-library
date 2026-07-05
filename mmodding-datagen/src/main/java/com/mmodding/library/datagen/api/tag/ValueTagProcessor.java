7463836a1ddf4248567363be5f9f6579c923b9cf
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2026-07-05 15:00:25 +0200

    Add ValueTagProcessor Utility Methods

--- mmodding-datagen/src/main/java/com/mmodding/library/datagen/api/tag/ValueTagProcessor.java
+++ mmodding-datagen/src/main/java/com/mmodding/library/datagen/api/tag/ValueTagProcessor.java
@@ -10,6 +10,31 @@ import java.util.function.Function;
 public interface ValueTagProcessor<T> {
 
 	/**
+	 * Processes the elements by generating them for the given tag.
+	 * @param tag the given tag
+	 * @return the value tag processor
+	 * @param <T> the element type
+	 */
+	static <T> ValueTagProcessor<T> forTag(TagKey<T> tag) {
+		return (provider, element) -> provider.apply(tag).add(element);
+	}
+
+	/**
+	 * Processes the elements by generating them for the given tags.
+	 * @param tags the given tags
+	 * @return the value tag processor
+	 * @param <T> the element type
+	 */
+	@SafeVarargs
+	static <T> ValueTagProcessor<T> forTags(TagKey<T>... tags) {
+		return (provider, element) -> {
+			for (TagKey<T> tag : tags) {
+				provider.apply(tag).add(element);
+			}
+		};
+	}
+
+	/**
 	 * Processes elements by appending them to preconfigured tags
 	 * @param appenderProvider provider function to retrieve tag appender objects
 	 * @param element the element to process
