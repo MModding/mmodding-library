0bfcc241bd31abb680548e3cd1dfe18fed50a625
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2026-07-15 07:37:56 +0200

    Fix #9 Make MModdingLibrary#getModContainer work on Data Generation Entrypoints

--- mmodding-core/src/main/java/com/mmodding/library/core/impl/MModdingInitializer.java
+++ mmodding-core/src/main/java/com/mmodding/library/core/impl/MModdingInitializer.java
@@ -5,6 +5,7 @@ import dev.yumi.commons.event.EventManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
@@ -16,16 +17,23 @@ public class MModdingInitializer implements ModInitializer {
 
 	public static final EventManager<Identifier> EVENT_MANAGER = new EventManager<>(MModdingLibrary.createId("default"), Identifier::parse);
 
-	private static final Map<String, Class<?>> SUPPORTED_ENTRYPOINT_TYPES = Map.of("main", ModInitializer.class, "client", ClientModInitializer.class, "server", DedicatedServerModInitializer.class);
+	private static final Map<String, Class<?>> SUPPORTED_ENTRYPOINT_TYPES = Map.of(
+		"main", ModInitializer.class,
+		"client", ClientModInitializer.class,
+		"server", DedicatedServerModInitializer.class,
+		"fabric-datagen", DataGeneratorEntrypoint.class
+	);
 
 	@Override
 	public void onInitialize() {}
 
 	public static ModContainer getModContainer(Class<?> entrypoint) {
 		for (Map.Entry<String, Class<?>> entrypointType : SUPPORTED_ENTRYPOINT_TYPES.entrySet()) {
-			for (EntrypointContainer<?> container : FabricLoader.getInstance().getEntrypointContainers(entrypointType.getKey(), entrypointType.getValue())) {
-				if (container.getEntrypoint().getClass().equals(entrypoint)) {
-					return container.getProvider();
+			if (entrypointType.getValue().isAssignableFrom(entrypoint)) {
+				for (EntrypointContainer<?> container : FabricLoader.getInstance().getEntrypointContainers(entrypointType.getKey(), entrypointType.getValue())) {
+					if (container.getEntrypoint().getClass().equals(entrypoint)) {
+						return container.getProvider();
+					}
 				}
 			}
 		}
